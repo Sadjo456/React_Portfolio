ecbb3b0071193bf4adf021f3fdbd15c56d2889c5
hassimioudiallo <hddinguiraye@gmail.com>
2023-04-20 00:50:47 -0400

    added css to about me component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contacts from "./components/Contact";
 
 export default function App() {
     return (
-        <main>
+        <main className= "text-gray-400 bg-gray-900 body-font">
             <Navbar />
             <AboutMe />
             <Projcts />
