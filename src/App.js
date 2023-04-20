28aad25f05da89410f0e9b5c49f50081866a2be7
hassimioudiallo <hddinguiraye@gmail.com>
2023-04-20 00:14:26 -0400

    import components App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import AboutMe from "./components/AboutMe";
+import Navbar from "./components/Navbar";
+import Projcts from "./components/Projcts";
+import Contacts from "./components/Contact";
+
+
 export default function App() {
     return (
         <main>
